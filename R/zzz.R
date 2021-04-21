95c7f73506d88783b91a10390431a487e1afdce7
Maximilian Held <info@maxheld.de>
2021-04-22 00:37:00 +0200

    disable just disc cache

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
 .onLoad <- function(libname, pkgname) {
   can_cache <- requireNamespace("memoise", quietly = TRUE) &
     requireNamespace("cachem", quietly = TRUE)
-  if (FALSE) {
+  if (can_cache) {
     verb_doi <<- memoise::memoise(
-      retry_doi,
-      cache = getOption(
-        "biblids.cache",
-        default = cachem::cache_mem(max_size = 1024 * 1024^2)
-      )
+      retry_doi
+      # cache = getOption(
+      #   "biblids.cache",
+      #   default = cachem::cache_mem(max_size = 1024 * 1024^2)
+      # )
     )
   }
 }
