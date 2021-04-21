3c353ec8c1d4bba8e1f11f36bd694c056935e1c9
Maximilian Held <info@maxheld.de>
2021-04-22 00:44:56 +0200

    remove disc cache

--- R/zzz.R
+++ R/zzz.R
@@ -2,12 +2,6 @@
   can_cache <- requireNamespace("memoise", quietly = TRUE) &
     requireNamespace("cachem", quietly = TRUE)
   if (can_cache) {
-    verb_doi <<- memoise::memoise(
-      retry_doi
-      # cache = getOption(
-      #   "biblids.cache",
-      #   default = cachem::cache_mem(max_size = 1024 * 1024^2)
-      # )
-    )
+    verb_doi <<- memoise::memoise(retry_doi)
   }
 }
