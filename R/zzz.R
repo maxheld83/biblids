0c0d422970c375733da089a51f93f4dd5428cabb
Maximilian Held <info@maxheld.de>
2021-07-21 16:10:16 +0200

    use proper test

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   if (can_cache) {
     verb_doi <<- memoise::memoise(retry_doi)
   }
-  if (require("shiny")) {
+  if (requireNamespace("shiny", quietly = TRUE)) {
     options(wama.default.app = doiEntryApp())
   }
 }
